692024546d234eb3b4f8bb61361661eed7a1f661
Bektursun <samatuulu11@gmail.com>
2019-12-09 20:54:57 +0600

    Creates model Quote and does settings admin page of Django

--- source/lab_quote/admin.py
+++ source/lab_quote/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from lab_quote.models import Quote
 
-# Register your models here.
+
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'text', 'created_at', 'author_name', 'author_email', 'rating', 'status']
+    list_filter = ['created_at', 'author_name']
+    search_fields = ['text', 'author_name']
+    fields = ['pk', 'text', 'created_at', 'author_name', 'author_email', 'rating', 'status']
+    readonly_fields = ['created_at']
+
+
+admin.site.register(Quote, QuoteAdmin)
+
--- source/lab_quote/models.py
+++ source/lab_quote/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+QUOTE_NEW = 'new'
+QUOTE_APPROVED = 'approved'
+QUOTE_STATUS_CHOICES = (
+    (QUOTE_NEW, 'New'),
+    (QUOTE_APPROVED, 'Approved')
+)
+
+
+class Quote(models.Model):
+    text = models.TextField(max_length=2000, verbose_name='Quotes')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created at")
+    author_name = models.CharField(max_length=50, verbose_name='Who added?')
+    author_email = models.EmailField(verbose_name='Email')
+    rating = models.IntegerField(default=0, verbose_name='Rating')
+    status = models.CharField(max_length=20, choices=QUOTE_STATUS_CHOICES, default=QUOTE_NEW, verbose_name='Status')
+
+    def __str__(self):
+        return self.text[:20] + '...'
+
+    class Meta:
+        verbose_name = 'Quote'
+        verbose_name_plural = 'Quotes'
